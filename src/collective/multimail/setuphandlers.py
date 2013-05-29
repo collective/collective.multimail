19b9fb38cdac4fab16872ded627e80a48ff6717a
Adam Terrey <arterrey@pretaweb.com>
2013-05-29 15:46:00 +1000

    fix to implementedBy bug

--- src/collective/multimail/setuphandlers.py
+++ src/collective/multimail/setuphandlers.py
@@ -6,31 +6,34 @@ from interfaces import IMultiMailHost
 _STASH_NAME = "collective.multimail-MailHost_stashed"
 
 
-### Setup of mailhost - stash current mail host...
 
-def setupPreToolSet(context):
+def setupVerious(context):
     if not context.readDataFile('multimailhost_various.txt'):
         return
     stash_MailHost(context)
+    createtool_MultiMailHost(context)
+    save_stashed_MailHost(context)
+
+
 
 def stash_MailHost(context):
     portal = getToolByName(context._site, 'portal_url').getPortalObject()
     # let's not atempt recousion
-    if type(IMultiMailHost.implementedBy(portal["MailHost"])) != MultiMailHost:
+    if type(portal["MailHost"]) != MultiMailHost:
         portal._setObject(_STASH_NAME, portal["MailHost"])
 
 
-### Save the stashed mailhost into the multimailhost...
+def createtool_MultiMailHost(context):
+    portal = getToolByName(context._site, 'portal_url').getPortalObject()
+    if type(portal["MailHost"]) != MultiMailHost:
+        portal._delObject("MailHost")
+        portal._setObject("MailHost", MultiMailHost())
 
-def setupPostToolSet(context):
-    if not context.readDataFile('multimailhost_various.txt'):
-        return
-    save_stashed_MailHost(context)
 
 def save_stashed_MailHost(context):
     portal = getToolByName(context._site, 'portal_url').getPortalObject()
     if _STASH_NAME in portal:
-        portal["MailHost"]._setObject('MailHost_default', _STASH_NAME)
+        portal["MailHost"]._setObject('MailHost_default', portal[_STASH_NAME])
         portal._delObject(_STASH_NAME)
 
 
